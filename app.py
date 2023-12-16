c53c29e7cae96a21e0159e53b2cf213257834aad
Piyush <128150406+elixir1001@users.noreply.github.com>
2023-12-16 13:29:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import streamlit as st
 from prediction import prediction_function
 from sentiment import sentiment_function
 
-
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 column1, column2 = st.columns(2)
 with column1:
   prediction_function()
