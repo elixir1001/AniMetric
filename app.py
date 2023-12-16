a7e34442574d0d68bf6e29ec3f98b0aa7b882a9d
Piyush <128150406+elixir1001@users.noreply.github.com>
2023-12-16 13:29:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ from sentiment import sentiment_function
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
     page_icon="🧊",
-    layout="wide",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    layout="wide"
 )
 column1, column2 = st.columns(2)
 with column1:
